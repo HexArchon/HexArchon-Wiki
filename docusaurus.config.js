3e85032fa5b5fc35cc46ac035f19c1118b127bfe
HexArchon <hexarchon@gmail.com>
2022-06-30 03:45:52 -0500

    Domain setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://HexArchon.github.io',
-  baseUrl: '/HexArchon-Wiki/',
+  url: 'http://newwiki.hexarchon.net/',
+  baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
